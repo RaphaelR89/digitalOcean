300b03db67f3f27f8c583ac8e3948dc7d90068b4
Raphael Rocha <rapharocha89@vidrelo.onmicrosoft.com>
2022-09-25 23:55:16 -0300

    feat:useState

--- state-hook-tutorial/src/components/Product/Product.jsx
+++ state-hook-tutorial/src/components/Product/Product.jsx
@@ -10,32 +10,52 @@ function getTotal(total) {
   return total.toLocaleString(undefined, currencyOptions);
 }
 
+const products = [
+  {
+    emoji: "🍦",
+    name: "ice cream",
+    price: 5,
+  },
+  {
+    emoji: "🍩",
+    name: "donuts",
+    price: 2.5,
+  },
+  {
+    emoji: "🍉",
+    name: "watermelon",
+    price: 4,
+  },
+];
+
 export default function Product() {
   const [cart, setCart] = useState([]);
   const [total, setTotal] = useState(0);
+// console.log(cart);
+// console.log(total)
+  function add(product) {
+    setCart((current) => [...current, product.name]);
 
-  function add() {
-    setCart(["ice cream"]);
-    setTotal(5);
+    setTotal((current) => current + product.price);
   }
   return (
     <div className="wrapper">
       <div>Shopping Cart: {cart.length} total items.</div>
       <div>Total: {getTotal(total)}</div>
-      <div className="product">
-        <span role="img" aria-label="ice cream">
-          🍦
-        </span>
+      <div>
+        {products.map((product) => (
+          <div key={product.name}>
+            <div className="product">
+              <span role="img" aria-label={product.name}>
+                {" "}
+                {product.emoji}
+              </span>
+            </div>
+            <button onClick={() => add(product)}>Add</button>
+            <button>Remove</button>
+          </div>
+        ))}
       </div>
-      <button onClick={add}>Add</button>{" "}
-      <button
-        onClick={() => {
-          setCart([]);
-          setTotal(0);
-        }}
-      >
-        Remove
-      </button>
     </div>
   );
 }
